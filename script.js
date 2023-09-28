7f72cfa67230d27ebf326a2ed9d8ec190fdd7ddc
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2023-09-28 16:32:32 +0200

    ajout description + barres dans la page à propos

--- script.js
+++ script.js
@@ -61,3 +61,13 @@ const scrollUp = () => {
 }
 
 document.addEventListener('scroll', scrollUp)
+
+
+
+// Compétences, affichage des barres
+
+$(document).ready(function(){
+	$('.progress .progress-bar').css("width", function(){
+		return $(this).attr("aria-valuenow") + "%";
+	})
+})
